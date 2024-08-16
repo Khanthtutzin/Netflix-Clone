cfa352ce26eda7ef1e2712d8c11e8123ee44587c
Khant Htut Zin <khanthtutzin@gmail.com>
2024-08-16 16:15:58 +0630

    Netflix Clone v1.1

--- src/Components/TitleCard/TitleCard.jsx
+++ src/Components/TitleCard/TitleCard.jsx
@@ -1,10 +1,20 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCard.css'
 import cards_datas from '../../assets/cards/Cards_data'
+import { Link } from 'react-router-dom';
 
 const TitleCard = ({title, category}) => {
 
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOTM4NmM1MGM3ZDhiMDdlNTBkZDM1ODRiMzIxMTY5ZiIsIm5iZiI6MTcyMzc5NjAzNy4wOTE4OTgsInN1YiI6IjY2YmYwNTZkMjE3ZDRmOWU3ZjUwZWE4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QQynfjudLjXVH469xI1tHs-Gh2d807vBESilL4c1NI0'
+    }
+  };
 
   const handlWheel = (event)=>{
     event.preventDefault();
@@ -12,6 +22,12 @@ const TitleCard = ({title, category}) => {
   }
 
   useEffect(()=>{
+
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results))
+    .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel',handlWheel);
   },[])
 
@@ -20,11 +36,11 @@ const TitleCard = ({title, category}) => {
     <div className='titlecard'>
       <h2>{title ? title:"Popular on Netflix"}</h2>
       <div className="card-lists" ref={cardsRef}>
-          {cards_datas.map((card,idx)=>{
-            return <div className='card' key={idx}>
-              <img src={card.image} alt="movie image" />
-              <p>{card.name}</p>
-            </div>
+          {apiData.map((card,idx)=>{
+            return <Link to={`/player/${card.id}`} className='card' key={idx}>
+              <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="movie_image" />
+              <p>{card.original_title}</p>
+            </Link>
           })}
       </div>
     </div>
