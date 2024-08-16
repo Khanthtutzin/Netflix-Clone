25394297eff70d65fa696229094c959eaf010d40
Khant Htut Zin <khanthtutzin@gmail.com>
2024-08-16 20:49:08 +0630

    issuse fixed

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import { toast } from "react-toastify";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDNI8ztxX5ZLr00LFrl9dVEhv5NJV4QSHI",
-  authDomain: "netflix-clone-17301.firebaseapp.com",
-  projectId: "netflix-clone-17301",
-  storageBucket: "netflix-clone-17301.appspot.com",
-  messagingSenderId: "476960578311",
-  appId: "1:476960578311:web:e737b417fe49b834d003a2"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
